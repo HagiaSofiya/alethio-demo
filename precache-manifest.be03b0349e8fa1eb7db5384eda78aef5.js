bb7dc5858ee6be1d6818315e116318a089145086
HagiaSofiya <sofiyashrayber@gmail.com>
2020-03-23 15:45:23 -0400

    Updates

---
+++ precache-manifest.be03b0349e8fa1eb7db5384eda78aef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fe9f241f28ee44dac9e0628af37896",
+    "url": "/alethio-demo/index.html"
+  },
+  {
+    "revision": "9ce053eda96db15e8aa5",
+    "url": "/alethio-demo/static/js/2.bf8387ed.chunk.js"
+  },
+  {
+    "revision": "1f2939b1b30bf47d431abe07c72a7f9e",
+    "url": "/alethio-demo/static/js/2.bf8387ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70198d99163f16589f8e",
+    "url": "/alethio-demo/static/js/main.e1569e0e.chunk.js"
+  },
+  {
+    "revision": "a10883d6cbbf1ce72095",
+    "url": "/alethio-demo/static/js/runtime-main.c12d440f.js"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/alethio-demo/static/media/google.695a3160.svg"
+  }
+]);
